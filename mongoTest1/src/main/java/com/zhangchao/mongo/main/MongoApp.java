d75b72ad5775c432caa62f0ca52bee7c9f759529
zhangchao <zhangchao@ejiajx.com>
2018-10-08 16:38:24 +0800

    1

--- mongoTest1/src/main/java/com/zhangchao/mongo/main/MongoApp.java
+++ mongoTest1/src/main/java/com/zhangchao/mongo/main/MongoApp.java
@@ -16,11 +16,13 @@ public class MongoApp {
     public static void main(String[] args){
         try {
             //连接mongo服务器(3后这么连接，传统uri mongodb:// 账号 ：密码 @ IP：端口号 / 数据库)
+            //如果没有账号密码
 //          MongoClientURI uri = new MongoClientURI("mongodb://localhost:27017/test");
+            //如果有账号密码
             MongoClientURI uri = new MongoClientURI("mongodb://zhangch:root@localhost:27017/test");
             MongoClient client = new MongoClient(uri);
             System.out.println("连接mongo服务器成功");
-            //连接数据库
+            //连接test数据库
             MongoDatabase db = client.getDatabase("test");
             System.out.println("连接test数据库成功"+db.getName());
             //当前数据库中所有集合
@@ -32,7 +34,7 @@ public class MongoApp {
             //向zhangchao集合插入一条数据
             Document document = new Document("name", "测试").append("age", 30).append("gender", "男");
             collection.insertOne(document);
-            //遍历zhangchao集合--- 1
+            //遍历zhangchao集合--- 方法1
             collection.find().forEach(new Block<Document>() {
                 @Override
                 public void apply(Document document) {
@@ -40,7 +42,7 @@ public class MongoApp {
                 }
             });
             System.out.println("--------------------------------------------------------------");
-            //遍历zhangchao集合--- 2
+            //遍历zhangchao集合--- 方法2
             MongoCursor<Document> iterator = collection.find().iterator();
             while ( iterator.hasNext() ){
                 System.out.println(iterator.next().toJson());
